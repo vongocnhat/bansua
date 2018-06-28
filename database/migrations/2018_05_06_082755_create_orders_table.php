e4d783383accda82bf09d8a542875a3f65ea4705
vongocnhat <nhatdn96it@gmail.com>
2018-06-28 19:23:31 +0700

    edit orders migration user_id nullable

--- database/migrations/2018_05_06_082755_create_orders_table.php
+++ database/migrations/2018_05_06_082755_create_orders_table.php
@@ -15,7 +15,7 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('user_id')->nullable();
             $table->string('name')->nullable();
             $table->string('email', 191)->nullable();
             $table->string('phone', 20)->nullable();
